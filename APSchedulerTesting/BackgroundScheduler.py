09d2ff13da34b496458a97eee0af4fb899b1ff3f
gao le <jilaqijilaqi@qq.com>
2021-07-18 15:22:53 +0800

    Basic Scheduler learning and testing. Block and background tested.

--- APSchedulerTesting/BackgroundScheduler.py
+++ APSchedulerTesting/BackgroundScheduler.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor
 import datetime
 import os
@@ -25,17 +25,16 @@ if __name__ == '__main__':
         'coalesce': False,
         'max_instances': 3  # 这里是限制单个函数能被执行的次数上限。
     }
-    scheduler = BlockingScheduler(executor=executor, job_defaults=job_defaults)
+    scheduler = BackgroundScheduler(executor=executor, job_defaults=job_defaults)
     scheduler.add_job(say_hello, 'interval', seconds=3)
     scheduler.add_job(another_hello, 'interval', seconds=3)
+    scheduler.start()
+
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-
     try:
-        scheduler.start()
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(5)
     except (KeyboardInterrupt, SystemExit):
-        pass
-    except:
-        print('Other exception!')
-
-
-
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        scheduler.shutdown()
---
+++ APSchedulerTesting/notes.rst
@@ -0,0 +1,43 @@
+scheduler种类
+#################
+
+BlockingScheduler：
+    Scheduler是唯一的运行内容
+
+BackgroundScheduler:
+    在不使用任何其他框架的时候，scheduler在后台运行的情况，选这个。
+
+AsyncIOScheduler：
+    如果使用了asyncio模块，用这个
+
+GeventSchduler：
+    如果使用了Gevent模块。
+
+TornadoSchdeuler：
+    如果是窗见一个Tornaod服务器应用。
+
+
+
+几个测试结果
+=============
+
+BlockingScheduler情况下，ThreadPoolExecutor，例子见BlockingScheduler：
+
+    * 对于单个函数任务，不做任何配置，任何函数在同一时刻只能被调用一次。如果上一次没有结束，会报错。
+    * job_defaults中的max_instances限制了单个函数被执行的上限次数。如果想限制数量，设置为1。
+
+BackgroundScheduler与BlockingScheduler不一样在于需要程序保持运行，保证background能存在。例子见BackgrounScheduler。
+
+
+特殊需求
+============
+
+对于函数执行完后，callback如何处理：
+
+    add_listener
+
+对于tornado的scheduler，如何使用：
+
+
+对于动态管理任务，如何进行：
+
